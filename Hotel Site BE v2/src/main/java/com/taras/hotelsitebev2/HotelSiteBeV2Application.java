0ed2c79d0ec5b043edc4e8a9f74beb3cf5433ed7
Javier Gorostiaga <gorostiagaj@yahoo.com>
2023-03-29 11:33:43 -0400

    Initial Project Add

---
+++ Hotel Site BE v2/src/main/java/com/taras/hotelsitebev2/HotelSiteBeV2Application.java
@@ -0,0 +1,13 @@
+package com.taras.hotelsitebev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSiteBeV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelSiteBeV2Application.class, args);
+    }
+
+}
---
+++ Hotel Site BE v2/src/test/java/com/taras/hotelsitebev2/HotelSiteBeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taras.hotelsitebev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSiteBeV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
